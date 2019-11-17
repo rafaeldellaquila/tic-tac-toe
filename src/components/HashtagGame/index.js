df671f2517298671a4c708e4a2860fe133380ebc
Rafael <rafadellaquila@gmail.com>
2019-11-16 23:41:19 -0300

    create dynimic results for player clicks

--- src/components/HashtagGame/index.js
+++ src/components/HashtagGame/index.js
@@ -1,42 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 import BoardGame from "../../objects/BoardGame";
 import PlayerGame from "../../objects/PlayerGame";
 
-const HashtagGame = () => (
-  <BoardGame>
-    <ul className="hashtag-game">
-      <li className="item">
-        <PlayerGame player="ex" />
-      </li>
-      <li className="item">
-        <PlayerGame player="os" />
-      </li>
-      <li className="item">
-        <PlayerGame player="ex" />
-      </li>
+const HashtagGame = () => {
+  const [nextPlayer, setNextPlayer] = useState("ex");
+  const [players, setPlayers] = useState([
+    { id: 1, content: "" },
+    { id: 2, content: "" },
+    { id: 3, content: "" },
+    { id: 4, content: "" },
+    { id: 5, content: "" },
+    { id: 6, content: "" },
+    { id: 7, content: "" },
+    { id: 8, content: "" },
+    { id: 9, content: "" }
+  ]);
 
-      <li className="item">
-        <PlayerGame player="ex" />
-      </li>
-      <li className="item">
-        <PlayerGame player="os" />
-      </li>
-      <li className="item">
-        <PlayerGame player="ex" />
-      </li>
+  const handleClick = id => {
+    setPlayers(old =>
+      old.map(player =>
+        player.id === id ? { id, content: nextPlayer } : player
+      )
+    );
 
-      <li className="item">
-        <PlayerGame player="os" />
-      </li>
-      <li className="item">
-        <PlayerGame player="os" />
-      </li>
-      <li className="item">
-        <PlayerGame player="os" />
-      </li>
-    </ul>
-  </BoardGame>
-);
+    setNextPlayer(old => (old === "ex" ? "os" : "ex"));
+  };
+
+  return (
+    <BoardGame>
+      <ul className="hashtag-game">
+        {players.map(({ id, content }) => (
+          <li
+            key={id}
+            className="item"
+            onClick={() => content === "" && handleClick(id)}
+          >
+            <PlayerGame id={id} content={content} />
+          </li>
+        ))}
+      </ul>
+    </BoardGame>
+  );
+};
 
 export default HashtagGame;
--- src/objects/PlayerGame/index.js
+++ src/objects/PlayerGame/index.js
@@ -1,15 +1,20 @@
 import React from "react";
+import "./styles.css";
 import Ex from "../../img/Ex.png";
 import Os from "../../img/Os.png";
 
-const PlayerGame = ({ player }) => {
+const PlayerGame = ({ content = "" }) => {
   let players = [];
+  players[""] = "";
   players["ex"] = Ex;
   players["os"] = Os;
 
   return (
     <button className="player-game">
-      <img src={players[player]} alt={`Player ${player.toUpperCase()}`} />
+      {players[content] && (
+        <img src={players[content]} alt={`Player ${content}`} />
+      )}
+      {/* Usando && direto pois disparar o segundo trecho apenas se for true se nao retorna vazio */}
     </button>
   );
 };
