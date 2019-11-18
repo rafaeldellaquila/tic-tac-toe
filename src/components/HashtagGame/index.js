f5443c222beed19f209705f1f852d780f0125f80
Rafael <rafadellaquila@gmail.com>
2019-11-17 21:30:59 -0300

    start make state for players

--- src/components/HashtagGame/index.js
+++ src/components/HashtagGame/index.js
@@ -4,46 +4,40 @@ import BoardGame from "../../objects/BoardGame";
 import PlayerGame from "../../objects/PlayerGame";
 
 const HashtagGame = ({ callback }) => {
-  const [nextPlayer, setNextPlayer] = useState("x");
-  const [players, setPlayers] = useState([
-    { id: 1, content: "" },
-    { id: 2, content: "" },
-    { id: 3, content: "" },
-    { id: 4, content: "" },
-    { id: 5, content: "" },
-    { id: 6, content: "" },
-    { id: 7, content: "" },
-    { id: 8, content: "" },
-    { id: 9, content: "" }
-  ]);
+    const [nextPlayer, setNextPlayer] = useState("x");
+    const [players, setPlayers] = useState([
+        { id: 1, content: "" },
+        { id: 2, content: "" },
+        { id: 3, content: "" },
+        { id: 4, content: "" },
+        { id: 5, content: "" },
+        { id: 6, content: "" },
+        { id: 7, content: "" },
+        { id: 8, content: "" },
+        { id: 9, content: "" }
+    ]);
 
-  const handleClick = id => {
-    setPlayers(old =>
-      old.map(player =>
-        player.id === id ? { id, content: nextPlayer } : player
-      )
+    const handleClick = id => {
+        setPlayers(old =>
+            old.map(player => (player.id === id ? { id, content: nextPlayer } : player))
+        );
+
+        callback(nextPlayer);
+
+        setNextPlayer(old => (old === "x" ? "o" : "x"));
+    };
+
+    return (
+        <BoardGame>
+            <ul className="hashtag-game">
+                {players.map(({ id, content }) => (
+                    <li key={id} className="item" onClick={() => content === "" && handleClick(id)}>
+                        <PlayerGame id={id} content={content} />
+                    </li>
+                ))}
+            </ul>
+        </BoardGame>
     );
-
-    callback(nextPlayer);
-
-    setNextPlayer(old => (old === "x" ? "o" : "x"));
-  };
-
-  return (
-    <BoardGame>
-      <ul className="hashtag-game">
-        {players.map(({ id, content }) => (
-          <li
-            key={id}
-            className="item"
-            onClick={() => content === "" && handleClick(id)}
-          >
-            <PlayerGame id={id} content={content} />
-          </li>
-        ))}
-      </ul>
-    </BoardGame>
-  );
 };
 
 export default HashtagGame;
